186af32e9f272c0f179b0c2c356a48061c1fe3f1
paujoko <paujoko@gmail.com>
2026-01-03 10:52:49 +0100

    Initial Spring Boot + Vaadin setup

---
+++ src/test/java/ch/hsg/supermarket/mvcsupermarket/MvcSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.hsg.supermarket.mvcsupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSupermarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
